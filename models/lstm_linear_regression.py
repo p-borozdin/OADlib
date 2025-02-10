bf2ecbe5cd5d6fc05fd3ca233aae96e284e64327
p-borozdin <p.borozdin.bsnss@gmail.com>
2025-02-10 15:44:05 +0700

    make LSTMLinearRegression inherit BaseLSTMModel class

--- models/lstm_linear_regression.py
+++ models/lstm_linear_regression.py
@@ -3,12 +3,12 @@ with the Linear Regression block after the LSTM block
 """
 import torch
 
-from OADlib.models.lstm_block import LSTMBlock
+from OADlib.models.base_lstm_model import BaseLSTMModel
 
 
-class LSTMLinearRegression(torch.nn.Module):
+class LSTMLinearRegression(BaseLSTMModel):
     """ LSTM-based model with Linear Regression block as its head.\n
-    The class inherits from torch.nn.Module
+    The class inherits from BaseLSTMModel
     """
     def __init__(
             self,
@@ -24,42 +24,18 @@ class LSTMLinearRegression(torch.nn.Module):
             hidden_size (int): size of hidden state in a LSTM cell
             device (torch.device): CPU or GPU torch.device
         """
-        super(LSTMLinearRegression, self).__init__()
-
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.device = device
-
-        self.blocks = torch.nn.ParameterDict()
-        self._init_blocks()
-
-    def _init_blocks(self):
-        """ Initialization of the model's blocks (LSTM and head blocks)
-        """
-        blocks = {}
-
-        blocks['lstm'] = self._init_lstm_block()
-        blocks['head'] = self._init_head_block()
-
-        self.blocks = torch.nn.ParameterDict(blocks)
-
-    def _init_lstm_block(self) -> torch.nn.Module:
-        """ Initialize the model's LSTM block
-
-        Returns:
-            torch.nn.Module: LSTM block for the model
-        """
-        return LSTMBlock(
-            input_size=self.input_size,
-            hidden_size=self.hidden_size,
-            device=self.device
-        )
+        super().__init__(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            device=device
+            )
 
     def _init_head_block(self) -> torch.nn.Module:
-        """ Initialize the model's head block
+        """ Initialize the model's head block.\n
+        Overriding the method from the base class.
 
         Returns:
-            torch.nn.Module: head block for the model
+            torch.nn.Module: Linear Regression head block for the model
         """
         head_layers: list[torch.nn.Module] = []
 
@@ -72,19 +48,3 @@ class LSTMLinearRegression(torch.nn.Module):
             )
 
         return torch.nn.ParameterList(head_layers)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """ Forward pass for LSTM-based model with Linear Regression block
-
-        Args:
-            x (torch.Tensor): input tensor
-
-        Returns:
-            torch.Tensor: output tensor
-        """
-        lstm_output = self.blocks['lstm'](x)
-        output = lstm_output[:, -1, :]
-        for layer in self.blocks['head']:
-            output = layer(output)
-
-        return output

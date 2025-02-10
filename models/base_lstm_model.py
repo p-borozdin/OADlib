9d0eeb0aed1e0da98196e57b18ebda7c270dba28
p-borozdin <p.borozdin.bsnss@gmail.com>
2025-02-10 15:42:25 +0700

    add BaseLSTMModel class implementing the common functionality for all the concrete LSTM-based model impls

---
+++ models/base_lstm_model.py
@@ -0,0 +1,77 @@
+""" The Module provides the architecture of a base LSTM-based model
+"""
+import torch
+
+from OADlib.models.lstm_block import LSTMBlock
+
+
+class BaseLSTMModel(torch.nn.Module):
+    """ Base class for all LSTM-based models.\n
+    The class inherits from torch.nn.Module
+    """
+    def __init__(
+            self,
+            input_size: int,
+            hidden_size: int,
+            device: torch.device
+            ):
+        """ Model's initialization
+
+        Args:
+            input_size (int): number of consecutive points used to make a \
+                  prediction
+            hidden_size (int): size of hidden state in a LSTM cell
+            device (torch.device): CPU or GPU torch.device
+        """
+        super(BaseLSTMModel, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.device = device
+
+        self.blocks = torch.nn.ParameterDict()
+        self.__init_blocks()
+
+    def __init_blocks(self):
+        """ Initialization of the model's blocks (LSTM and head blocks)
+        """
+        blocks = {}
+
+        blocks['lstm'] = LSTMBlock(
+            input_size=self.input_size,
+            hidden_size=self.hidden_size,
+            device=self.device
+            )
+        blocks['head'] = self._init_head_block()
+
+        self.blocks = torch.nn.ParameterDict(blocks)
+
+    def _init_head_block(self) -> torch.nn.Module:
+        """ Initialize the model's head block.\n
+        Virtual method, must be overriden in all derived classes.
+
+        Raises:
+            NotImplementedError: method was called from the base class
+
+        Returns:
+            torch.nn.Module: head block for the model
+        """
+        raise NotImplementedError()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ Default forward pass for LSTM-based model,
+        when only the last LSTM cell's results are taken.\n
+        Optionally can be overriden in derived classes.
+
+        Args:
+            x (torch.Tensor): input tensor
+
+        Returns:
+            torch.Tensor: output tensor
+        """
+        lstm_output = self.blocks['lstm'](x)
+        output = lstm_output[:, -1, :]
+        for layer in self.blocks['head']:
+            output = layer(output)
+
+        return output
